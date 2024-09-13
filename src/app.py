7ddc2961f85bb83c85d1c794400891eb378abcbd
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2024-09-13 07:25:15 -0300

    Versao mais basica do Gerenciador de Arquivos

---
+++ src/app.py
@@ -0,0 +1,44 @@
+# Importando bibliotecas para gerenciamento/manipulação de arquivos e diretórios
+import os
+import shutil
+
+
+# Diretório que será organizado
+diretorio = "/home/thalles/Documentos/"
+
+
+# Variáveis com os nomes dos diretórios que serão criados
+powerPoint = "PowerPoint"
+imagens = "Imagens"
+documentos = "Word"
+pdf1 = "PDF"
+sheets = "Planilhas"
+textos = "TXT"
+
+# Lista de diretórios
+listDirectory = [powerPoint, imagens, documentos, pdf1, sheets, textos]
+
+# Criando os diretórios
+    # for dir in listDirectory:
+    #     # Comando para criar Diretórios
+    #     os.mkdir(diretorio+dir)
+    #     print(diretorio+dir)
+
+# Listando os diretórios e arquivos
+arquivos = os.listdir(diretorio)
+    # print(arquivos)
+
+# Organizando os arquivos em cada diretório respectivo à extensão
+for file in arquivos:
+    if file.lower().endswith((".pptx")):
+        shutil.move(os.path.join(diretorio, file), os.path.join(diretorio, powerPoint, file))
+    elif file.lower().endswith((".png", ".jpg")):
+        shutil.move(os.path.join(diretorio, file), os.path.join(diretorio, imagens, file))
+    elif file.lower().endswith((".docx", ".odt")):
+        shutil.move(os.path.join(diretorio, file), os.path.join(diretorio, documentos, file))
+    elif file.lower().endswith((".pdf")):
+        shutil.move(os.path.join(diretorio, file), os.path.join(diretorio, pdf1, file))
+    elif file.lower().endswith((".xlsx", ".ods", ".csv")):
+        shutil.move(os.path.join(diretorio, file), os.path.join(diretorio, sheets, file))
+    elif file.lower().endswith((".txt")):
+        shutil.move(os.path.join(diretorio, file), os.path.join(diretorio, textos, file))
